50af2b2b2e741d62b2e78937db9f0b9d6a42208b
sam <sjwyss@outlook.com>
2025-03-25 09:50:39 -0400

    swapped to using LLVM style

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -10,40 +10,36 @@
  * formats and prints error messages
  * @param msg error message
  */
-inline void error_msg(const std::string& msg)
-{
-    fmt::print(fg(fmt::color::crimson), "[ERROR]");
-    fmt::print(": {}\n", msg);
+inline void error_msg(const std::string &msg) {
+  fmt::print(fg(fmt::color::crimson), "[ERROR]");
+  fmt::print(": {}\n", msg);
 };
 
 /*!
  * formats and prints warning messages
  * @param msg warning message
  */
-inline void warning_msg(const std::string& msg)
-{
-    fmt::print(fg(fmt::color::orange), "[WARNING]");
-    fmt::print(": {}\n", msg);
+inline void warning_msg(const std::string &msg) {
+  fmt::print(fg(fmt::color::orange), "[WARNING]");
+  fmt::print(": {}\n", msg);
 };
 
 /*!
  * formats and prints info messages
  * @param msg info message
  */
-inline void info_msg(const std::string& msg)
-{
-    fmt::print(fg(fmt::color::green), "[INFO]");
-    fmt::print(": {}\n", msg);
+inline void info_msg(const std::string &msg) {
+  fmt::print(fg(fmt::color::green), "[INFO]");
+  fmt::print(": {}\n", msg);
 };
 
 /*!
  * formats and prints logging messages
  * @param msg logging message
  */
-inline void log_msg(const std::string& msg)
-{
-    fmt::print(fg(fmt::color::blue), "[LOG]");
-    fmt::print(": {}\n", msg);
+inline void log_msg(const std::string &msg) {
+  fmt::print(fg(fmt::color::blue), "[LOG]");
+  fmt::print(": {}\n", msg);
 };
 
 /*!
@@ -51,13 +47,12 @@ inline void log_msg(const std::string& msg)
  * @param rank MPI rank on which error occurred
  * @param msg error message
  */
-inline void abort_err(const int rank, const std::string& msg)
-{
-    // log out error message
-    error_msg("Aborted on rank: " + fmt::to_string(rank) + ". Reason: " + msg);
+inline void abort_err(const int rank, const std::string &msg) {
+  // log out error message
+  error_msg("Aborted on rank: " + fmt::to_string(rank) + ". Reason: " + msg);
 
-    // terminate mpi environment
-    MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+  // terminate mpi environment
+  MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
 };
 
 #endif //UTILS_UTILS_HPP
