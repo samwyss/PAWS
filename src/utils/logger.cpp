4b926e7e2d2d6b1f78f57fc720d75046d2c26abb
sam <sjwyss@outlook.com>
2025-03-25 22:42:55 -0400

    started to flesh out logger

--- src/utils/logger.cpp
+++ src/utils/logger.cpp
@@ -1,5 +1,46 @@
-//
-// Created by sam on 3/25/25.
-//
+#include "logger.hpp"
 
-#include "Logger.h"
+Logger::Logger(const LoggerLevel level,
+               const LoggerRanks ranks) : level_(level), ranks_(ranks) {
+
+}
+
+Logger::~Logger() = default;
+
+void Logger::log(const LoggerLevel level, const LoggerRanks ranks,
+                 const int rank, const std::string &msg) {
+
+}
+
+void Logger::emerg(const int rank, const std::string &msg) {
+
+}
+
+void Logger::alert(const int rank, const std::string &msg) {
+
+}
+
+void Logger::crit(const int rank, const std::string &msg) {
+
+}
+
+void Logger::err(const int rank, const std::string &msg) {
+
+}
+
+void Logger::warning(const int rank, const std::string &msg) {
+
+}
+
+void Logger::info(const int rank, const std::string &msg) {
+
+}
+
+void Logger::debug(const int rank, const std::string &msg) {
+
+}
+
+
+
+
+
--- src/utils/logger.hpp
+++ src/utils/logger.hpp
@@ -1,16 +1,51 @@
-//
-// Created by sam on 3/25/25.
-//
+#ifndef UTILS_LOGGER_H
+#define UTILS_LOGGER_H
 
-#ifndef LOGGER_H
-#define LOGGER_H
+#include <fmt/core.h>
+#include <fmt/color.h>
+#include <mpi.h>
+#include <string>
 
+/*!
+ * logger level enum according to the Syslog standard
+ */
+enum class LoggerLevel {
+  emerg,
+  alert,
+  crit,
+  err,
+  warning,
+  notice,
+  info,
+  debug,
+};
 
+/*!
+ * logger rank enum used to specify if all ranks are to write or just rank zero
+ */
+enum class LoggerRanks {
+  zero,
+  all,
+};
 
 class Logger {
+public:
+  Logger(LoggerLevel level, LoggerRanks ranks);
+  ~Logger();
+  void log(LoggerLevel level, LoggerRanks ranks, int rank,
+           const std::string &msg);
 
+private:
+  void emerg(int rank, const std::string &msg);
+  void alert(int rank, const std::string &msg);
+  void crit(int rank, const std::string &msg);
+  void err(int rank, const std::string &msg);
+  void warning(int rank, const std::string &msg);
+  void notice(int rank, const std::string &msg);
+  void info(int rank, const std::string &msg);
+  const LoggerLevel level_;
+  const LoggerRanks ranks_;
 };
 
 
-
-#endif //LOGGER_H
+#endif //UTILS_LOGGER_H
