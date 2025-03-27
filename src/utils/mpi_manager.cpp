706472f85c5663faec05c1a502e06aa3364771ea
sam <sjwyss@outlook.com>
2025-03-27 17:05:01 -0400

    deleted redundant files

--- src/utils/mpi_manager.cpp
+++
@@ -1,8 +0,0 @@
-#include "mpi_manager.h"
-
-MPIManager::MPIManager(int &argc, char **argv) {
-  MPI_Init(&argc, &argv);
-  comm = MPI_COMM_WORLD;
-  MPI_Comm_rank(comm, &rank);
-  MPI_Comm_size(comm, &size);
-}
