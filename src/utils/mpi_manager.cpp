47bdbd2495c0af489ab3f547381fe5f9ab71a853
sam <sjwyss@outlook.com>
2025-03-27 08:59:34 -0400

    added mpi manager struct

---
+++ src/utils/mpi_manager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sam on 3/27/25.
+//
+
+#include "mpi_manager.h"
---
+++ src/utils/mpi_manager.h
@@ -0,0 +1,16 @@
+//
+// Created by sam on 3/27/25.
+//
+
+#ifndef MPI_MANAGER_H
+#define MPI_MANAGER_H
+
+
+
+class mpi_manager {
+
+};
+
+
+
+#endif //MPI_MANAGER_H
